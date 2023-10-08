ac2a478f002221a6a00c0f387dca6595b4575d16
mirfani340 <muhammadirfani340@gmail.com>
2023-10-08 08:55:24 +0700

    update bash script

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # Build a image
+echo "Build images"
 docker build -t shipping-service:v1 .
 
 # Renamning the docker images to ghcr format
-docker tag shipping-service:v1 ghcr.io/mirfani340/devops-dicoding-3:v1
+echo "Renaming the images"
+docker tag shipping-service:v1 ghcr.io/mirfani340/devops-dicoding-3/shipping-services:v1
 
 # Login to GHCR via custom variable and then using --password-stdin
+echo "Login to GHCR"
 echo $DOCKER_GHCR_TOKEN | docker login ghcr.io -u mirfani340 --password-stdin
 
 # Push to GHCR
-docker push ghcr.io/mirfani340/devops-dicoding-3:v1
+echo "Pushing to GHCR..."
+docker push ghcr.io/mirfani340/devops-dicoding-3/shipping-services:v1
